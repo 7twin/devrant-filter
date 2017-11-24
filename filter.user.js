db6d75ace4006ffc238c676fdf1363e2b84f1761
7twin <32747235+7twin@users.noreply.github.com>
2017-11-24 18:06:02 +0100

    Rename filter.js to filter.user.js

--- filter.user.js
+++ filter.user.js
@@ -1,40 +1,40 @@
 // ==UserScript==
 // @name         Devrant Filter
-// @namespace    https://devrant.com/
+// @namespace    
 // @version      0.1
 // @description  a filter for devrant comments and rants
 // @author       7twin
-// @match        https://devrant.com/rants/*
-// @match        https://devrant.com/feed/
-// @match        https://devrant.com/notifs/
+// @match        
+// @match        
+// @match        
 // @grant        none
-// @require      http://code.jquery.com/jquery-latest.js
+// @require      
 // ==/UserScript==
 
 $(window).load(function(){
     var filter_arr = [""]; // Add blacklisted (case-sensitive) nicknames here
     var compiled_regex = new RegExp('<div class="rant-username">('+ filter_arr.join("|") +')<\/div>', 'g');
 
-    if(window.location.href == "https://devrant.com/feed/"){
+    if(window.location.href == ""){
         // filter rants on frontpage
         $(".rantlist-title").each(function(){
             var rant = $(this);
-            $.get("https://devrant.com"+$(this).attr("href"),function(data){
+            $.get("{
                 if(compiled_regex.test(data)){
                     rant.closest(".rant-comment-row-widget").remove();
                 }
             });
         });
-    }else if(window.location.href.indexOf("https://devrant.com/rants/") > -1){
+    }else if(window.location.href.indexOf("") > -1){
         // filter comments in rants
         $.map(filter_arr,function(n){
             // remove comments
             $('li[data-username="'+n+'"]').remove();
 
             // remove all messages mentioning them
-            $(".rantlist-title:contains('@"+n+"')").closest(".reply-row").remove();
+            $(".").closest(".reply-row").remove();
         });
-    }else if(window.location.href == "https://devrant.com/notifs/"){
+    }else if(window.location.href == ""){
         // filter notifications
         setInterval(function(){
             $.map(filter_arr,function(n){
